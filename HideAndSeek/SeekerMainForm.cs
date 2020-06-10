f4ee73b8d0e525758e0bde7cfaafc852773ebb0a
Youri Peusen <youri-peusen@hotmail.com>
2020-06-10 02:41:19 +0200

    First Forms set up

--- HideAndSeek/SeekerMainForm.cs
+++ HideAndSeek/SeekerMainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace HideAndSeek
 {
-	public partial class Form1 : Form
+	public partial class SeekerMainForm : Form
 	{
-		public Form1()
+		public SeekerMainForm()
 		{
 			InitializeComponent();
 		}
---
+++ HideAndSeek/StartForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HideAndSeek
+{
+	public partial class StartForm : Form
+	{
+		public StartForm()
+		{
+			InitializeComponent();
+		}
+
+		private void StartForm_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnSeeker_Click(object sender, EventArgs e)
+		{
+			this.Hide();
+
+			SeekerMainForm form = new SeekerMainForm();
+			form.Closed += (s, args) => this.Close();
+			form.Show();
+		}
+
+		private void btnHider_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
